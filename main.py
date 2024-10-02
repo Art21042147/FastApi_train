641a87323089df1d6bf597d225647dee8a7a0049
Art21042147 <art21042147@gmail.com>
2024-10-02 13:49:29 +0300

    Main Page

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def welcome():
     return {"message": "Hello World"}
+
+@app.get("/main")
+async def welcome() -> dict:
+    return {"message": "Main Page"}
