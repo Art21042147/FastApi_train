cff5b86f05aaef2e943b3b393db6cbe7829f65e9
Art21042147 <art21042147@gmail.com>
2024-10-02 14:37:55 +0300

    Routing

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ async def welcome():
 async def welcome() -> dict:
     return {"message": "Main Page"}
 
+@app.get("/user/Jonathan/Livingston")
+async def news() -> dict:
+    return {"message": "Hello, QA!"}
+
+@app.get("/id")
+async def id_login(username: str = "Art", age: int = 24) -> dict:
+    return {"User": username, "Age": age}
+
 @app.get("/user/{first_name}/{last_name}")
 async def news(first_name: str, last_name: str) -> dict:
     return {"message": f"Hello, {first_name} {last_name}"}
