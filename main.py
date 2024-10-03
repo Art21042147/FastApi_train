c9bbe700f0dc804e6541426aa168cf2bd3b9063a
Art21042147 <art21042147@gmail.com>
2024-10-03 10:32:40 +0300

    Имитация работы с БД

--- main.py
+++ main.py
@@ -1,34 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
-messages_db = {"0": "First post in FastAPI"}
+users = {'1': 'Имя: Example, возраст: 18'}
 
-@app.get("/")
-async def get_all_messages() -> dict:
-    return messages_db
+@app.get("/users")
+async def get_users():
+    return users
 
-@app.get("/message/{message_id}")
-async def get_message(message_id: str) -> dict:
-    return messages_db[message_id]
+@app.post("/user/{username}/{age}")
+async def post_user(
+    username: Annotated[str, Path(min_length=1, description="Enter Name", example="John")],
+    age: Annotated[int, Path(ge=18, description="Enter Age", example=18)]):
+    user_id = str(max(map(int, users.keys())) + 1)
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {user_id} is registered"
 
-@app.post("/message")
-async def create_message(message: str) -> str:
-    current_index = str(int(max(messages_db, key=int)) + 1)
-    messages_db[current_index] = message
-    return "New message created"
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(
+    user_id: Annotated[str, Path(description="ID of the user to update")],
+    username: Annotated[str, Path(min_length=1, description="Updated username")],
+    age: Annotated[int, Path(ge=18, description="Updated age")]):
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"The user {user_id} has been updated"
 
-@app.put("/message/{message_id}")
-async def update_message(message_id: str, message: str) -> str:
-    messages_db[message_id] = message
-    return "Message updated"
-
-@app.delete("/message/{message_id}")
-async def delete_message(message_id: str) -> str:
-    messages_db.pop(message_id)
-    return f"Message {message_id} deleted"
-
-@app.delete("/")
-async def delete_all_messages() -> str:
-    messages_db.clear()
-    return "All messages deleted"
+@app.delete("/user/{user_id}")
+async def delete_user(
+    user_id: Annotated[str, Path(description="ID of the user to delete")]):
+    del users[user_id]
+    return f"User {user_id} has been deleted"
