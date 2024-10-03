45131d28db8aa9c8ce301dd5a57064df0855cb6a
Art21042147 <art21042147@gmail.com>
2024-10-03 15:58:05 +0300

    Модель пользователя

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
+from pydantic import BaseModel
 from typing import Annotated
 
 app = FastAPI()
 
-users = {'1': 'Имя: Example, возраст: 18'}
+users = []
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
 
 @app.get("/users")
 async def get_users():
     return users
 
+
 @app.post("/user/{username}/{age}")
 async def post_user(
-    username: Annotated[str, Path(min_length=1, description="Enter Name", example="John")],
-    age: Annotated[int, Path(ge=18, description="Enter Age", example=18)]):
-    user_id = str(max(map(int, users.keys())) + 1)
-    users[user_id] = f"Имя: {username}, возраст: {age}"
-    return f"User {user_id} is registered"
+        username: Annotated[str, Path(min_length=1, description="Enter Name", example="John")],
+        age: Annotated[int, Path(ge=18, description="Enter Age", example=18)]):
+    user_id = users[-1].id + 1 if users else 1
+
+    new_user = User(id=user_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
+
 
 @app.put("/user/{user_id}/{username}/{age}")
 async def update_user(
-    user_id: Annotated[str, Path(description="ID of the user to update")],
-    username: Annotated[str, Path(min_length=1, description="Updated username")],
-    age: Annotated[int, Path(ge=18, description="Updated age")]):
-    users[user_id] = f"Имя: {username}, возраст: {age}"
-    return f"The user {user_id} has been updated"
+        user_id: Annotated[int, Path(description="ID of the user to update")],
+        username: Annotated[str, Path(min_length=1, description="Updated username")],
+        age: Annotated[int, Path(ge=18, description="Updated age")]):
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
+
 
 @app.delete("/user/{user_id}")
 async def delete_user(
-    user_id: Annotated[str, Path(description="ID of the user to delete")]):
-    del users[user_id]
-    return f"User {user_id} has been deleted"
+        user_id: Annotated[int, Path(description="ID of the user to delete")]):
+    for user in users:
+        if user.id == user_id:
+            users.remove(user)
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
